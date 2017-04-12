2087e27b4b53c04b3491c33d819424b5e1034769
Kim Maida <kim.maida@auth0.com>
2017-04-12 11:30:02 -0400

    Add server.

--- server/server.js
+++ server/server.js
@@ -1,26 +1,32 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var cors = require('cors');
 var jwt = require('express-jwt');
 var jwks = require('jwks-rsa');
-
-var port = process.env.PORT || 3001;
+var dragonsJson = require('./dragons.json');
+var AUTH = require('./auth-variables.js');
 
 var jwtCheck = jwt({
     secret: jwks.expressJwtSecret({
         cache: true,
         rateLimit: true,
         jwksRequestsPerMinute: 5,
-        jwksUri: "https://kmaida.auth0.com/.well-known/jwks.json"
+        jwksUri: `https://${AUTH.CLIENT_DOMAIN}/.well-known/jwks.json`
     }),
     audience: 'http://localhost:3001/api/',
-    issuer: "https://kmaida.auth0.com/",
+    issuer: `https://${AUTH.CLIENT_DOMAIN}/`,
     algorithms: ['RS256']
 });
 
-app.use(jwtCheck);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+//app.use(jwtCheck);
 
-app.get('/api/authorized', function (req, res) {
-  res.send('Secured Resource');
+app.get('/api/dragons', jwtCheck, function (req, res) {
+  res.json(dragonsJson);
 });
 
-app.listen(port);
+app.listen(3001);
+console.log('Listening on localhost:3001');
