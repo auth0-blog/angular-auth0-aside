114e0a0e3b775c680c2e8a46ab4c30e5ce2de138
Kim Maida <kim.maida@auth0.com>
2017-11-27 11:19:38 -0500

    Update @angular/cli to v1.5.4 and FIX RxJS import in API service.

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 
 @Injectable()
